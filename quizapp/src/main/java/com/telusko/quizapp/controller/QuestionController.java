29cef9558ef39da8d65604fc32ac247adcd9dff7
sunnyraj <mrsj5714@gmail.com>
2026-05-17 16:31:42 +0530

    project file updated

--- quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -3,24 +3,31 @@ package com.telusko.quizapp.controller;
 import com.telusko.quizapp.Question;
 import com.telusko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+
     @Autowired
     QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions() {
+    public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
+
     @GetMapping("category/{category}")
-    public List<Question> getQuestionByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
         return questionService.getQuestionByCategory(category);
     }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
 }
